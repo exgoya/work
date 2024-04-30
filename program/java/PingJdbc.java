39df147e9ac97dc063c9e927244984e07972d42f
exgoya <exgoya@github.com>
2024-04-30 12:49:09 +0900

    240430

--- program/java/PingJdbc.java
+++ program/java/PingJdbc.java
@@ -24,25 +24,12 @@ public class PingJdbc
     public static void main(String[] args) throws SQLException,InterruptedException
     {
         Connection con = createConnectionByDriverManager("TEST", "test");
-<<<<<<< HEAD
-        PreparedStatement pstmt1 = con.prepareStatement("alter session set trace_long_run_timer = 1");
-        pstmt1.execute();
-        pstmt1.close();
-        PreparedStatement pstmt2 = con.prepareStatement("alter session set trace_long_run_cursor = 10");
-        pstmt2.execute();
-        pstmt2.close();
-        PreparedStatement pstmt3 = con.prepareStatement("alter session set trace_long_run_sql = 10");
-        pstmt3.execute();
-        pstmt3.close();
-        PreparedStatement pstmt = con.prepareStatement("SELECT 'PING' FROM DUAL");
-=======
         Statement stmt = con.createStatement();
         stmt.execute("alter session set trace_long_run_timer = 1");
         stmt.execute("alter session set trace_long_run_cursor = 10");
         stmt.execute("alter session set trace_long_run_sql = 10");
        //PreparedStatement pstmt = con.prepareStatement("SELECT 'PING' FROM DUAL");
         PreparedStatement pstmt = con.prepareStatement("SELECT * FROM t1");
->>>>>>> b4a4432ce125652bbd78707082294912666bbc1a
         long startTime = 0;
         long endTime = 0;
         long timeElapsed = 0;
@@ -54,11 +41,7 @@ public class PingJdbc
             rs = pstmt.executeQuery();
             while (rs.next())
             {
-<<<<<<< HEAD
-                Thread.sleep(1000);
-=======
 			//    Thread.sleep(500); //1초 대기
->>>>>>> b4a4432ce125652bbd78707082294912666bbc1a
                 System.out.println("ID = : " + rs.getString(1));
             }
             endTime = System.nanoTime();
